45440e6eac6ad7bce011e2c77a15e09e47d42d11
xuzhihao <yzxuzhihao@foxmail.com>
2018-06-07 12:38:36 +0800

    PAT_7-2_一元多项式的乘法与加法运算

--- 2_线性结构/练习题/PAT_7-2_一元多项式的乘法与加法运算.c
+++ 2_线性结构/练习题/PAT_7-2_一元多项式的乘法与加法运算.c
@@ -42,36 +42,39 @@ int main(){
 	pm = polyMultFun(p1,p2);
 	
 	/*输出多项式*/ 
-	printPoly(ps);
 	printPoly(pm); 
+	printPoly(ps);
 	return 0;
 } 
 
 /*输出多项式*/ 
 void printPoly(Polynomial ps){
 	int flag = 0;
+	if (!ps) {
+		printf("0 0\n");
+		return;
+	}
+		
 	while(ps){
 		if(!flag){
-			printf(" ");
-		}else{
 			flag = 1;
+		}else{
+			printf(" ");
 		}
-		
 		printf("%d %d",ps->coef,ps->expon);
-		flag ++;
 		ps = ps->link;
 	}
 	printf("\n");
 }
  
 /*多项式加法运算*/
-Polynomial polySumFun(Polynomial p1, Polynomial p2){
+Polynomial polySumFun(Polynomial t1, Polynomial t2){
 	//构造结果多项式链表头空节点
-	Polynomial rear,t,front;
+	Polynomial rear,t,front,p1,p2;
 	rear = (Polynomial)malloc(sizeof(struct Polynode));
 	rear -> link = null; 
-	front = rear; 
-	
+	front = rear;
+	p1 = t1;p2=t2;
 	while(p1 && p2){
 		if(p1->expon == p2->expon){
 			int c = p1->coef + p2->coef;
@@ -115,7 +118,7 @@ Polynomial polyMultFun(Polynomial p1, Polynomial p2){
 	ps = (Polynomial)malloc(sizeof(struct Polynode));
 	ps -> link = null; 
 	front = ps; 
-	
+	rear = ps;
 	//构造一个结果多项式p1的第一个节点乘以p2    //coef : 系数//expon : 指数
 	while(t2){
 		c = t1->coef * t2->coef;
@@ -126,7 +129,7 @@ Polynomial polyMultFun(Polynomial p1, Polynomial p2){
 	t1 = t1->link;
 	
 	while(t1){
-		t2 = p2;rear = ps;
+		t2 = p2; rear = ps;
 		while(t2){
 			c = t1->coef * t2->coef;
 			e = t1->expon + t2->expon;
@@ -134,31 +137,27 @@ Polynomial polyMultFun(Polynomial p1, Polynomial p2){
 				rear = rear->link;
 			}
 			//节点插入结果多项式
-			if(e == rear->expon){ /*指数相等,只需修改系数*/
-				if(c + rear->coef){
-					rear->coef = c + rear->coef;
+			if( rear->link && e == rear->link->expon){ /*指数相等,只需修改系数*/
+				if(c + rear->link->coef){
+					rear->link->coef += c;
 				}else{
 					/*删除该节点*/
-					t = rear;
-					if(rear -> link){
-						rear = rear -> link;
-					}
+					t = rear->link;				
+					rear->link = t->link;
 					free(t);
 				}
-			}else if(){/*指数小于rear->expon ,构造新节点插入*/ 
-				
-				
-			} 
-			
-			
+			}else{/*指数小于rear->expon ,构造新节点插入*/ 
+				t = (Polynomial)malloc(sizeof(struct Polynode));
+				t->coef = c ;
+				t->expon =e ;
+				t->link = rear->link;
+				rear->link = t;
+				rear = t;
+			} 	
 			t2=t2->link;
 		}
 		t1 = t1->link;
-	}
-	
-	
-	
-	
+	}	
 	//释放空节点
 	t = front;
 	front = front -> link;
